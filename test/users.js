d7ec685c9b141817d26f6b473002e65a5d86b62f
Prabuselvan2022 <prabuelamuruguselvan@gmail.com>
2023-05-09 01:48:04 +0530

    callback added

--- test/users.js
+++ test/users.js
@@ -6,12 +6,12 @@ import {expect} from 'chai';
 const TOKEN='f3138f305f6e84619669cc5a429ccd3702207b0e66a1c35be28a984dc44fd0ca';
 
 describe('User- First Test', () => {
-    it('GET/Users', () => {
+    it('GET/Users', (done) => {
         request.get(`users?access-token=${TOKEN}`).end((err, res)=> {
             console.log('error is ',err);
             console.log('response is ', res.body);
-
             expect(res.body.data).to.not.be.empty;
+            done(); // callback for waiting the test
         })
 
     });
